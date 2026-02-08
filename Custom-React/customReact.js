2aa8e08b9a995fdb6f774c760fa837c2978edbc0
Vipul Chandra Mishra <paritoshmishra5105@gmail.com>
2026-02-09 02:44:55 +0530

    Add custom react

---
+++ Custom-React/customReact.js
@@ -0,0 +1,20 @@
+function customRender(reactElement,container){
+    const domEle=document.createElement(reactElement.type)
+    domEle.innerHTML=reactElement.children
+    domEle.setAttribute('href',reactElement.props.href)
+    domEle.setAttribute('target',reactElement.props.target)
+
+    container.appendChild(domEle)  
+}
+
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children: 'click me to visit'
+}
+
+const mainContainer= document.querySelector('#root');
+customRender(reactElement,mainContainer)
